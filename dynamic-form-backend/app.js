9c61f1e5d0b6dab37988836f0bb175fc46424708
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 21:16:00 +0530

    final touch

--- dynamic-form-backend/app.js
+++ dynamic-form-backend/app.js
@@ -11,12 +11,18 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",                    
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
