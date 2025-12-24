bb0238048d570d4bd6ed38d1f85c0fee1c4ca07e
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 21:20:37 +0530

    final touch

--- dynamic-form-backend/config/db.js
+++ dynamic-form-backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection failed", err);
+    console.log("MongoDB connection failed", err);
     process.exit(1);
   }
 };
