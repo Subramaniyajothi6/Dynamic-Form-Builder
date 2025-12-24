57cea4ba65602e46d149a4052f0746748a04f316
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 17:27:10 +0530

    updated the baseUrl that is deployed

--- dynamic-form-backend/server.js
+++ dynamic-form-backend/server.js
@@ -1,11 +1,20 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+  require("dotenv").config();
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
