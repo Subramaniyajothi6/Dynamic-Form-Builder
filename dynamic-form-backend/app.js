4cd1c066e36fca70095eddfac7db7096101fa272
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 19:03:48 +0530

    final touch

--- dynamic-form-backend/app.js
+++ dynamic-form-backend/app.js
@@ -10,6 +10,7 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
