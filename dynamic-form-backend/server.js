e6811c0ff1e356d9203cd6f29ffeb0e9320e0041
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 18:37:25 +0530

    final touch

--- dynamic-form-backend/server.js
+++ dynamic-form-backend/server.js
@@ -1,12 +1,30 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+// server.js
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
-connectDB();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const start = async () => {
+  await connectDB();                // use the config/db.js function
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
+start();
