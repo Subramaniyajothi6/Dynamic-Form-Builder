1b3d5d9bd264d484d3ac2cad5f6904a8f9baeb29
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 17:00:13 +0530

    updated the baseUrl that is deployed

--- dynamic-form-backend/controllers/authController.js
+++ dynamic-form-backend/controllers/authController.js
@@ -2,13 +2,40 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// const register = async (req, res) => {
+//   try {
+//     const { name, email, password, role } = req.body;
+
+//     if (!email || !password) {
+//       return res.status(400).json({ message: "Email and password required" });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const user = await User.create({
+//       name,
+//       email,
+//       password: hashedPassword,
+//       role
+//     });
+
+//     res.status(201).json({ message: "User registered" });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
 const register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, role } = req.body || {};
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password required" });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({
+    await User.create({
       name,
       email,
       password: hashedPassword,
