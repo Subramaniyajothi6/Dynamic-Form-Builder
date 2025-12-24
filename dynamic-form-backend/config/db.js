4f1f0c2d0f16a72d29e91e7bddd21ac9c93f7bba
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-24 17:23:08 +0530

    updated the baseUrl that is deployed

--- dynamic-form-backend/config/db.js
+++ dynamic-form-backend/config/db.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    console.error("MongoDB connection failed", error);
+    throw error;
   }
 };
 
